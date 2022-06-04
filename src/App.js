5df185a56edb03c59acb60ed60d3649220e014b4
shirsho-roy <shirsho.tffs@gmail.com>
2022-06-04 18:51:58 +0530

    Header Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
 
+import "./App.css";
+import Header from "./pages/header/Header";
 function App() {
-  return <div className="App">{/* Router */}</div>;
+  return <div className="App">
+  <Header />
+  </div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors:{
+      'cyanhead': '#35b8be',
+      'notice-text':'hsla(0, 0%, 100%, 0.8)',
+      'white': '#fff', 
+      'col2' : '#333333'
+    },
+    fontSize:{
+      's': '24px',
+      's2': '16px',
+      's3': '18px'
+    },
+    minHeight:{
+      'cart':'55px',
+      'cart2': '50px'
+    },
+    minWidth:{
+      'cart': '55px',
+    },
+    boxShadow:{
+      'xl': '7px 7px 25px 0 rgb(126 130 143 / 27%)',
+    },
   },
   plugins: [],
 };
