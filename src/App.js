8a8c610b3fd456f11c64c6ac81ed3be5ba62af5b
shirsho-roy <shirsho.tffs@gmail.com>
2022-06-07 19:34:31 +0530

    Resolved Queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import "./App.css";
 import Header from "./pages/header/Header";
 function App() {
   return <div className="App">
-  <Header />
-  </div>;
+    <Header />
+  </div>
 }
 
 export default App;
